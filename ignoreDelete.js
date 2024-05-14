7d924e01128b53af13e0c2d632f431d16eef5e9a
Ryan Frazier <ryan.frazier@snyk.io>
2024-05-14 11:34:24 -0400

    remove hard coded ARN, clean up formatting

--- ignoreDelete.js
+++ ignoreDelete.js
@@ -1,48 +1,49 @@
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
 
 export const handler = async (event) => {
-  const snsClient = new SNSClient({});
-  const details = event.detail;
-  const content = details.content;
-  
-  const ignoredByUserId = details.actingUserPublicId;
-  const actingOrgPublicId = details.actingOrgPublicId;
-  const projectId = details.actingProjectPublicId
-  const issueId = content.issueId;
-  const reason = content.reason;
-  const reasonType = content.reasonType;
-  const eventTime = event.time;
-  const ignoreCreated = content.created;
-  const ignoreExpires = content.expires;
+    const snsTopicARN = "<SNS_TOPIC_ARN>" // add in SNS topic arn
+    const snsClient = new SNSClient({});
+    const details = event.detail;
+    const content = details.content;
 
-  const message = `Snyk Ignore Deleted Alert!
-  * Ignore Deleted By: ${ignoredByUserId}
-  * Organization: ${actingOrgPublicId}
-  * Project ID: ${projectId}
-  * Issue ID: ${issueId}
-  * Deleted Reason: ${reason}
-  * Deleted Reason Type: ${reasonType}
-  * Event Time: ${eventTime}
-  * Original Ignore Created: ${ignoreCreated}
-  * Original Ignore Expires: ${ignoreExpires}`;
+    const ignoredByUserId = details.actingUserPublicId;
+    const actingOrgPublicId = details.actingOrgPublicId;
+    const projectId = details.actingProjectPublicId
+    const issueId = content.issueId;
+    const reason = content.reason;
+    const reasonType = content.reasonType;
+    const eventTime = event.time;
+    const ignoreCreated = content.created;
+    const ignoreExpires = content.expires;
 
-  const params = {
-    Message: message,
-    Subject: "Snyk Ignore Deleted Alert",
-    TopicArn: "arn:aws:sns:us-east-1:590183812647:audit-log"
-  };
-  
-  const command = new PublishCommand(params);
+    const message = `Snyk Ignore Deleted Alert!
+        * Ignore Deleted By: ${ignoredByUserId}
+        * Organization: ${actingOrgPublicId}
+        * Project ID: ${projectId}
+        * Issue ID: ${issueId}
+        * Deleted Reason: ${reason}
+        * Deleted Reason Type: ${reasonType}
+        * Event Time: ${eventTime}
+        * Original Ignore Created: ${ignoreCreated}
+        * Original Ignore Expires: ${ignoreExpires}`;
 
-  try {
-    const response = await snsClient.send(command)
-    console.log("Notification sent successfully: ", response);
-  } catch (error) {
-    console.error("Error sending notification:", error);
-  }
+    const params = {
+        Message: message,
+        Subject: "Snyk Ignore Deleted Alert",
+        TopicArn: snsTopicARN,
+    };
 
-  return {
-    statusCode: 200,
-    body: "Event processed successfully",
-  };
+    const command = new PublishCommand(params);
+
+    try {
+        const response = await snsClient.send(command)
+        console.log("Notification sent successfully: ", response);
+    } catch (error) {
+        console.error("Error sending notification:", error);
+    }
+
+    return {
+        statusCode: 200,
+        body: "Event processed successfully",
+    };
 };
--- ignoreEdit.js
+++ ignoreEdit.js
@@ -1,58 +1,59 @@
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
 
 export const handler = async (event) => {
-  const snsClient = new SNSClient({});
-  const details = event.detail;
-  const content = details.content;
+    const snsTopicARN = "<SNS_TOPIC_ARN>" // add in SNS topic arn
+    const snsClient = new SNSClient({});
+    const details = event.detail;
+    const content = details.content;
 
-  const ignoredByUserId = details.actingUserPublicId;
-  const actingOrgPublicId = details.actingOrgPublicId;
-  const projectId = details.actingProjectPublicId
-  const issueId = content.issueId;
-  const beforeReason = content.before[0].reason;
-  const beforeReasonType = content.before[0].reasonType;
-  const beforeIgnoreCreated = content.before[0].created;
-  const beforeIgnoreExpires = content.before[0].expires;
-  const afterReason = content.after[0].reason;
-  const afterReasonType = content.after[0].reasonType;
-  const afterIgnoreCreated = content.after[0].created;
-  const afterIgnoreExpires = content.after[0].expires;
-  const previouslyIgnoredBy = content.before[0].ignoredBy.id
-  const eventTime = event.time;
+    const ignoredByUserId = details.actingUserPublicId;
+    const actingOrgPublicId = details.actingOrgPublicId;
+    const projectId = details.actingProjectPublicId
+    const issueId = content.issueId;
+    const beforeReason = content.before[0].reason;
+    const beforeReasonType = content.before[0].reasonType;
+    const beforeIgnoreCreated = content.before[0].created;
+    const beforeIgnoreExpires = content.before[0].expires;
+    const afterReason = content.after[0].reason;
+    const afterReasonType = content.after[0].reasonType;
+    const afterIgnoreCreated = content.after[0].created;
+    const afterIgnoreExpires = content.after[0].expires;
+    const previouslyIgnoredBy = content.before[0].ignoredBy.id
+    const eventTime = event.time;
 
-  const message = `Snyk Ignore Edited Alert!
-  * Organization: ${actingOrgPublicId}
-  * Project ID: ${projectId}
-  * Issue ID: ${issueId}
-  * Ignored By: ${ignoredByUserId}
-  * Previously Ignored By: ${previouslyIgnoredBy}
-  * Reason: ${afterReason}
-  * Previous Reason: ${beforeReason}
-  * Reason Type: ${beforeReasonType}
-  * Previous Reason Type: ${afterReasonType}
-  * Ignore Created: ${afterIgnoreCreated}
-  * Previous Ignore Created: ${beforeIgnoreCreated}
-  * Ignore Expires: ${afterIgnoreExpires}
-  * Previous Ignore Expires: ${beforeIgnoreExpires}
-  * Event Time: ${eventTime}`;
+    const message = `Snyk Ignore Edited Alert!
+        * Organization: ${actingOrgPublicId}
+        * Project ID: ${projectId}
+        * Issue ID: ${issueId}
+        * Ignored By: ${ignoredByUserId}
+        * Previously Ignored By: ${previouslyIgnoredBy}
+        * Reason: ${afterReason}
+        * Previous Reason: ${beforeReason}
+        * Reason Type: ${beforeReasonType}
+        * Previous Reason Type: ${afterReasonType}
+        * Ignore Created: ${afterIgnoreCreated}
+        * Previous Ignore Created: ${beforeIgnoreCreated}
+        * Ignore Expires: ${afterIgnoreExpires}
+        * Previous Ignore Expires: ${beforeIgnoreExpires}
+        * Event Time: ${eventTime}`;
 
-  const params = {
-    Message: message,
-    Subject: `Snyk Ignore Alert for Issue: ${issueId}`,
-    TopicArn: "arn:aws:sns:us-east-1:590183812647:audit-log"
-  };
-  
-  const command = new PublishCommand(params);
+    const params = {
+        Message: message,
+        Subject: `Snyk Ignore Alert for Issue: ${issueId}`,
+        TopicArn: snsTopicARN,
+    };
 
-  try {
-    const response = await snsClient.send(command)
-    console.log("Notification sent successfully: ", response);
-  } catch (error) {
-    console.error("Error sending notification:", error);
-  }
+    const command = new PublishCommand(params);
 
-  return {
-    statusCode: 200,
-    body: "Event processed successfully",
-  };
+    try {
+        const response = await snsClient.send(command)
+        console.log("Notification sent successfully: ", response);
+    } catch (error) {
+        console.error("Error sending notification:", error);
+    }
+
+    return {
+        statusCode: 200,
+        body: "Event processed successfully",
+    };
 };
